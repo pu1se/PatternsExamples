f97c38d4f21c13f15c259de9e0b644b9f3587fbf
Pavel Pontus <pavel_pontus@tut.by>
2023-04-03 13:35:26 +0300

    more features added

--- ConsoleExample/LanguageFeatures/MainProgram.cs
+++ ConsoleExample/LanguageFeatures/MainProgram.cs
@@ -37,6 +37,44 @@ namespace ConsoleExample.LanguageFeatures
             }
 
             WriteLine();
+
+            var canIDoCombination = new Func<int, int, (bool canIDoCombination, int resultOfCombination)>
+                ((a,b) => (true, a+b));
+            var (canIDoThis, result2) = canIDoCombination(50, 10);
+            if (canIDoThis)
+                WriteLine($"50 + 10 = {result2}");
+
+            WriteLine();
+
+            var me = (Name: "Pasha", Surname: "Pontus");
+            WriteLine($"This is {me.Name} {me.Surname}");
+
+            WriteLine();
+
+
+            // c# 8 pattern matching. Use as validation:
+            var user = new User
+            {
+                Name = "Peter",
+                Age = 16,
+                Salary = 2000
+            };
+            var errorMessage = user switch
+            {
+                null => "Object is missing",
+                { Name: null } => "Name is missing",
+                { Age: 0 } => "Age is missing",
+                { Salary: 0 } => "Salary is missing",
+                { Age: var age } when age < 18 => "User must be older then 18.",
+                { Salary: var salary } when salary < 1000 => "Salary must be bigger then 1000",
+                _ => null
+            };
+            if (errorMessage != null)
+            {
+                WriteLine(errorMessage);
+            }
+
+            WriteLine();WriteLine();WriteLine();
         }
 
         static bool IsItPossibleToCombine(int a, int b, out int result)
@@ -53,6 +91,13 @@ namespace ConsoleExample.LanguageFeatures
         private class Shape
         {
             public int Width, Height;
+        }
+
+        private class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public decimal Salary { get; set; }
         }
     }
 }
--- ConsoleExample/Program.cs
+++ ConsoleExample/Program.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using ConsoleExample.Templates.Builder;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using static System.Console;
+
 
 namespace ConsoleExample
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             LanguageFeatures.MainProgram.Code();
             Templates.ObserverClassicEventBindingList.MainProgram.Code();
-
             //Templates.BuilderWithRecurciveGeneric.MainProgram.Code();
-
             //Templates.BuilderWithFluentApi.MainProgram.Code();
         }
     }

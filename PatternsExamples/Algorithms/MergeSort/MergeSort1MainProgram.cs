fa5ffac17833ed9728eb2bf580b9393d80e795ae
Pasha <pasha.parrrker@gmail.com>
2026-09-09 15:20:24 +0300

    added algorthms folder

---
+++ PatternsExamples/Algorithms/MergeSort/MergeSort1MainProgram.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsExamples.Algorithms.MergeSort
+{
+    internal class MergeSort1MainProgram
+    {
+    }
+}
---
+++ PatternsExamples/Algorithms/QuickSort/QuickSort1MainProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace PatternsExamples.Algorithms.QuickSort;
+
+public class QuickSort1MainProgram
+{
+    public void Run()
+    {
+
+    }
+
+    public int[] SortArray(int[] arr)
+    {
+
+    }
+}
+

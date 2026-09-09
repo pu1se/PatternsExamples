c67985ab22e366acff0650555cb1d9a3950cfde0
Pasha <pasha.parrrker@gmail.com>
2026-09-09 15:57:50 +0300

    added merge sort

--- PatternsExamples/Algorithms/MergeSort/MergeSort1MainProgram.cs
+++ PatternsExamples/Algorithms/MergeSort/MergeSort1MainProgram.cs
@@ -1,14 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using PatternsExamples._Core;
+﻿using PatternsExamples._Core;
 
-namespace PatternsExamples.Algorithms.MergeSort
+namespace PatternsExamples.Algorithms.MergeSort;
+
+internal class MergeSort1MainProgram : IMainProgram
 {
-    internal class MergeSort1MainProgram : IMainProgram
+    public void RunCode()
     {
-        public void RunCode()
+        var sort = new Solution();
+
+        int[] inputArray = [10, 9, 1, 1, 1, 2, 4, 3, 1];
+        var outputArray = sort.SortArray(inputArray);
+    }
+}
+
+file class Solution
+{
+    public int[] SortArray(int[] arr)
+    {
+        var atomicArrays = SplitArrayOnAtomicArrays(arr);
+        return SortArrayTreeUntilThereIsOneElement(atomicArrays)[0];
+    }
+
+    List<int[]> SortArrayTreeUntilThereIsOneElement(List<int[]> arrLeavs)
+    {
+        List<int[]> arrRoot = new();
+
+        if (arrLeavs.Count == 1)
+            return arrLeavs;
+
+        for (var index = 0; index < arrLeavs.Count; index += 2)
         {
+            if (index + 1 < arrLeavs.Count)
+            {
+                var mergedArray = MergeTwoSortedArraysIntoOne(arrLeavs[index], arrLeavs[index + 1]);
+                arrRoot.Add(mergedArray);
+            }
+            else
+            {
+                arrRoot.Add(arrLeavs[index]);
+            }
+
         }
+        return SortArrayTreeUntilThereIsOneElement(arrRoot);
+    }
+
+    int[] MergeTwoSortedArraysIntoOne(int[] leftArray, int[] rightArray)
+    {
+        var mergedArray = new List<int>();
+        var leftIndex = 0;
+        var rightIndex = 0;
+
+        while (leftIndex < leftArray.Length && rightIndex < rightArray.Length)
+        {
+            if (leftArray[leftIndex] <= rightArray[rightIndex])
+            {
+                mergedArray.Add(leftArray[leftIndex++]);
+            }
+            else
+            {
+                mergedArray.Add(rightArray[rightIndex++]);
+            }
+        }
+
+        if (leftIndex < leftArray.Length)
+        {
+            return mergedArray.Concat(leftArray.Skip(leftIndex)).ToArray();
+        }
+
+        return mergedArray.Concat(rightArray.Skip(rightIndex)).ToArray();
+    }
+
+    List<int[]> SplitArrayOnAtomicArrays(int[] arr)
+    {
+        List<int[]> atomicArrays = new(arr.Length);
+
+        for (var index = 0; index < arr.Length; index += 2)
+        {
+            atomicArrays.Add([arr[index]]);
+
+            if (index + 1 < arr.Length)
+                atomicArrays.Add([arr[index + 1]]);
+        }
+
+        return atomicArrays;
     }
 }
---
+++ PatternsExamples/Algorithms/MergeSort/MergeSort2MainProgram.cs
@@ -0,0 +1,61 @@
+﻿using PatternsExamples._Core;
+
+namespace PatternsExamples.Algorithms.MergeSort;
+
+internal class MergeSort2MainProgram : IMainProgram
+{
+    public void RunCode()
+    {
+        var sort = new Solution();
+
+        int[] inputArray = [10, 9, 1, 1, 1, 2, 4, 3, 1];
+        var outputArray = sort.SortArray(inputArray);
+    }
+}
+
+file class Solution
+{
+    public int[] SortArray(int[] arr)
+    {
+        var atomicArrays = MapOnAtoms(arr);
+        return ReduceTree(atomicArrays);
+    }
+
+    int[] ReduceTree(Queue<Queue<int>> arrQueue)
+    {
+        while (arrQueue.Count > 1)
+        {
+            var mergedArray = MergeTwoSortedArrays(arrQueue.Dequeue(), arrQueue.Dequeue());
+            arrQueue.Enqueue(mergedArray);
+        }
+
+        return arrQueue.Dequeue().ToArray();
+    }
+
+    Queue<int> MergeTwoSortedArrays(Queue<int> arrQueue1, Queue<int> arrQueue2)
+    {
+        var mergedArray = new Queue<int>();
+
+        while (arrQueue1.Count != 0 && arrQueue2.Count != 0)
+        {
+            var queueWithMin = arrQueue1.Peek() < arrQueue2.Peek()
+                               ? arrQueue1 : arrQueue2;
+
+            mergedArray.Enqueue(queueWithMin.Dequeue());
+        }
+
+        return new([.. mergedArray, .. arrQueue1, .. arrQueue2]);
+    }
+
+    Queue<Queue<int>> MapOnAtoms(int[] arr)
+    {
+        Queue<Queue<int>> atomics = new(arr.Length);
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            atomics.Enqueue(new Queue<int>([arr[i]]));
+        }
+
+        return atomics;
+    }
+}

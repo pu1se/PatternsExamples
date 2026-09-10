0714155bfaad9e18decb7abd7b1a5a231120ccd1
Pasha <pasha.parrrker@gmail.com>
2026-09-10 20:03:02 +0300

    working on 3sum

--- PatternsExamples/Algorithms/ThreeSum/ThreeSum2MainProgram.cs
+++ PatternsExamples/Algorithms/ThreeSum/ThreeSum2MainProgram.cs
@@ -5,11 +5,11 @@ namespace PatternsExamples.Algorithms.ThreeSum;
 //hint: не реализовано, есть только разбор
 // time  O(n^2) — цель
 // mem O(n) — цель
-internal class ThreeSum1MainProgram : IMainProgram
+internal class ThreeSum2MainProgram : IMainProgram
 {
     public void RunCode()
     {
-        int[] nums = [-1, 0, 1, 2, -1, -4];
+        int[] nums = [-2, 0, 0, 2, 2];
 
         var solution = new Solution();
         var triplets = solution.ThreeSum(nums);
@@ -23,19 +23,21 @@ file class Solution
     public List<List<int>> ThreeSum(int[] nums)
     {
         nums = nums.OrderBy(x => x).ToArray();
-        var treeplets = new List<List<int>>();
-        var hasSet = new HashSet<string>();
+        var triplets = new List<List<int>>();
 
-        for (int i = 0; i < nums.Length - 2; i++)
+        for (var i = 0; i < nums.Length - 2; i++)
         {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
             var left = i + 1;
             var right = nums.Length - 1;
 
-            while (left != right)
+            while (left < right)
             {
                 var sum = nums[i] + nums[left] + nums[right];
 
-                if (0 < sum)
+                if (sum > 0)
                     right--;
 
                 if (sum < 0)
@@ -43,19 +45,20 @@ file class Solution
 
                 if (sum == 0)
                 {
-                    if (hasSet.Add(GetHashSetKey(nums[i], nums[left], nums[right])))
-                    {
-                        treeplets.Add([nums[i], nums[left], nums[right]]);
-                    }
+                    triplets.Add([nums[i], nums[left], nums[right]]);
 
                     left++;
                     right--;
+
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
                 }
             }
         }
 
-        return treeplets;
+        return triplets;
     }
-
-    string GetHashSetKey(int first, int second, int theard) => $"{first}_{second}_{theard}";
 }
--- PatternsExamples/Program.cs
+++ PatternsExamples/Program.cs
@@ -1,4 +1,4 @@
 using PatternsExamples.Algorithms.ThreeSum;
 
-var mainProgram = new ThreeSum1MainProgram();
+var mainProgram = new ThreeSum2MainProgram();
 mainProgram.RunCode();
